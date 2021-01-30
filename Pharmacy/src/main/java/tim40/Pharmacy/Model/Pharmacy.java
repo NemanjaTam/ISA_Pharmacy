44d721a344511beb4768f1758957a6e7a53105eb
@cyberpoetry <sweetbee0071@gmail.com>
2021-01-30 23:20:19 +0100

    added pharmacy model

---
+++ Pharmacy/src/main/java/tim40/Pharmacy/Model/Pharmacy.java
@@ -0,0 +1,109 @@
+package tim40.Pharmacy.Model;
+
+import java.util.ArrayList;
+
+public class Pharmacy {
+
+    private String namePharmacy;
+    private String addressPharmacy;
+    private String descriptionPharmacy;
+    //  private ArrayList<Dermatologist> dermatologists;
+    //  private ArrayList<Pharmacist> pharmacists;
+    //  private ArrayList<Medicine> medicines;
+    //private ArrayList<Appointment> availableDermatologistAppointments;
+    // private ArrayList<PriceList> priceListMedicines;
+
+    public Pharmacy() {
+    }
+
+//    public Pharmacy(String namePharmacy, String addressPharmacy, String descriptionPharmacy, ArrayList<Dermatologist> dermatologists, ArrayList<Pharmacist> pharmacists, ArrayList<Medicine> medicines, ArrayList<Appointment> availableDermatologistAppointments, ArrayList<PriceList> priceListMedicines, ArrayList<PriceList> priceListAppointments) {
+//        this.namePharmacy = namePharmacy;
+//        this.addressPharmacy = addressPharmacy;
+//        this.descriptionPharmacy = descriptionPharmacy;
+//        this.dermatologists = dermatologists;
+//        this.pharmacists = pharmacists;
+//        this.medicines = medicines;
+//        this.availableDermatologistAppointments = availableDermatologistAppointments;
+//        this.priceListMedicines = priceListMedicines;
+//        this.priceListAppointments = priceListAppointments;
+//    }
+//
+
+
+    public String getAddressPharmacy() {
+        return addressPharmacy;
+    }
+
+    public String getNamePharmacy() {
+        return namePharmacy;
+    }
+
+    public String getDescriptionPharmacy() {
+        return descriptionPharmacy;
+    }
+
+//    public ArrayList<Dermatologist> getDermatologists() {
+//        return dermatologists;
+//    }
+//
+//    public ArrayList<Pharmacist> getPharmacists() {
+//        return pharmacists;
+//    }
+//
+//    public ArrayList<Medicine> getMedicines() {
+//        return medicines;
+//    }
+//
+//    public ArrayList<Appointment> getAvailableDermatologistAppointments() {
+//        return availableDermatologistAppointments;
+//    }
+//
+//    public ArrayList<PriceList> getPriceListMedicines() {
+//        return priceListMedicines;
+//    }
+//
+//    public ArrayList<PriceList> getPriceListAppointments() {
+//        return priceListAppointments;
+//    }
+
+
+    public void setNamePharmacy(String namePharmacy) {
+        this.namePharmacy = namePharmacy;
+    }
+
+    public void setAddressPharmacy(String addressPharmacy) {
+        this.addressPharmacy = addressPharmacy;
+    }
+
+    public void setDescriptionPharmacy(String descriptionPharmacy) {
+        this.descriptionPharmacy = descriptionPharmacy;
+    }
+
+//    public void setDermatologists(ArrayList<Dermatologist> dermatologists) {
+//        this.dermatologists = dermatologists;
+//    }
+//
+//    public void setPharmacists(ArrayList<Pharmacist> pharmacists) {
+//        this.pharmacists = pharmacists;
+//    }
+//
+//    public void setMedicines(ArrayList<Medicine> medicines) {
+//        this.medicines = medicines;
+//    }
+//
+//    public void setAvailableDermatologistAppointments(ArrayList<Appointment> availableDermatologistAppointments) {
+//        this.availableDermatologistAppointments = availableDermatologistAppointments;
+//    }
+//
+//    public void setPriceListMedicines(ArrayList<PriceList> priceListMedicines) {
+//        this.priceListMedicines = priceListMedicines;
+//    }
+//
+//    public void setPriceListAppointments(ArrayList<PriceList> priceListAppointments) {
+//        this.priceListAppointments = priceListAppointments;
+//    }
+//
+//    private ArrayList<PriceList> priceListAppointments;
+//
+//}
+}
