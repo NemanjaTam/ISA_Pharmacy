1daf8fd5d3f6a8d01668bc42ea12d8d06d7860db
Jovana Jakovljevic <jovanajako@hotmail.com>
2021-04-20 18:53:21 +0200

    Restructure app - Jovana

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <RegistrationPage/>
+  </div>
+</template>
+
+<script>
+import RegistrationPage from './views/RegistrationPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegistrationPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <nav>
+  <v-system-bar
+  height="30" color="red"
+></v-system-bar>
+    </nav>
+    <h1>This is an about page</h1>
+  </div>
+</template>
