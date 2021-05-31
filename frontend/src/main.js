8a40c2fe3d76889e3c74c9def0cb6c4fd933e6d3
antonic901 <antonicnikola6@gmail.com>
2021-05-31 21:47:14 +0200

    refactored frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { router } from './router'
+import { store } from './store'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,8 +11,8 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-new Vue({
-  router,
-  store,
+const app = new Vue({
+  router: router,
+  store: store,
   render: h => h(App)
 }).$mount('#app')
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,13 +3,49 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
+  //states (data) can be edided only through mutations
+  strict: true,
+  // this is like data:
   state: {
+    user: {
+      id: null,
+      name: "",
+      surname: "",
+      email: "",
+      phoneNumber: null,
+      gender: null,
+      birthdayDate: null,
+      website: "",
+      biography: "",
+      username: "",
+      password: ""
+   }
   },
+  //methods that return data (state)
+  getters: {
+    getFullName(state) {
+      return state.user.name + " " + state.user.surname
+    },
+    isUserLogged(state) {
+      if(state.user.id != null) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+  },
+  //methods for changing date (state)
   mutations: {
+    updateUser(state, {user}) {
+      state.user = user;
+    }
   },
+  //always on components dispatch action which commit some mutations. Never commit mutations from component because of async
   actions: {
-  },
-  modules: {
+    updateUser(context, {user}) {
+      context.commit('updateUser', {user})
+    }
   }
 })
