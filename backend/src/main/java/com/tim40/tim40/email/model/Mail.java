a15ca803f8dfd52a63a5ba9cfb1de0c82e01181c
antonic901 <antonicnikola6@gmail.com>
2021-06-06 23:57:39 +0200

    implemted email, but not working

---
+++ backend/src/main/java/com/tim40/tim40/email/model/Mail.java
@@ -0,0 +1,25 @@
+package com.tim40.tim40.email.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Mail {
+    private String mailFrom;
+    private String mailTo;
+    private String mailCc;
+    private String mailBcc;
+    private String mailSubject;
+    private String mailContent;
+    private String contentType;
+    private List <Object> attachments;
+}
