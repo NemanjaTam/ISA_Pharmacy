37ae601fbe21ed28909b0ea274bc2f3b62618c12
@cyberpoetry <sweetbee0071@gmail.com>
2021-08-31 12:43:57 +0200

    fix: price list not updating bug

--- frontend/src/components/PriceList.vue
+++ frontend/src/components/PriceList.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="background-price-list">
-      
+  <div >
+    <div class="background-price-list">
+    <b-table :items="this.getPriceListActive" :fields="fields" responsive="sm">
+      <template #cell(edit)="row">
+        <ModalPriceList
+          :selected="row.item"
+          :id="Pharmacy"
+          :prices="row.item.medicationPrices"
+          :type="userType"
+        ></ModalPriceList>
+      </template>
 
-      <b-table
-        :items="this.getPriceListActive"
+      <div></div>
+    </b-table>
+    </div>
+    <br/>
+   <div class="background-price-list">
+    <label> Inactive or not yet active price lists</label>
+     <b-table
+        :items="this.PriceListInactive"
         :fields="fields"
         responsive="sm"
       >
@@ -11,16 +26,15 @@
                   <ModalPriceList :selected="row.item" :id="Pharmacy" :prices="row.item.medicationPrices" :type="userType"></ModalPriceList>
             
         </template>
-
-        <div></div>
-      </b-table>
+     </b-table>
+        </div>
   </div>
 </template>
 <script>
-import ModalPriceList from "../components/ModalPriceList.vue"
+import ModalPriceList from "../components/ModalPriceList.vue";
 export default {
   name: "PriceList",
-  components: {ModalPriceList},
+  components: { ModalPriceList },
   computed: {
     userType() {
       return this.$store.getters.getUserType;
@@ -31,16 +45,16 @@ export default {
     Pharmacy() {
       return this.$store.getters.getPharmacy;
     },
-    getPriceListActive(){
-        return this.$store.getters.getPriceListActive;
+    getPriceListActive() {
+      return this.$store.getters.getPriceListActive;
     },
-    PriceListInactive(){
-        return this.$store.getters.getPriceListInactive;
-    }
+    PriceListInactive() {
+      return this.$store.getters.getPriceListInactive;
+    },
   },
   data() {
     return {
-         fields: [
+      fields: [
         {
           key: "startTime",
           sortable: true,
@@ -50,38 +64,61 @@ export default {
           label: "Status",
           sortable: true,
         },
-        "edit"
-
-        
-    ],
-  }},
-  methods:{
-      getActivePriceList(){
-        fetch(
-            `http://localhost:9005/api/pricelist/get-active-pricelist/${this.Pharmacy}`,
-            {
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                usertype: this.userType,
-              },
-              method: "GET",
-             
-            }
-          )
+        "edit",
+      ],
+    };
+  },
+  methods: {
+    getActivePriceList() {
+      fetch(
+        `http://localhost:9005/api/pricelist/get-active-pricelist/${this.Pharmacy}`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            usertype: this.userType,
+          },
+          method: "GET",
+        }
+      )
         .then(function(response) {
           if (response.ok) {
             return response.json();
           } else {
             return Promise.reject(response);
           }
-        }).then((data)=> this.$store.dispatch("updatePriceListActive", data))
+        })
+        .then((data) => this.$store.dispatch("updatePriceListActive", data))
         .catch();
-      }
+    },
+
+     getNonActivePriceList() {
+      fetch(
+        `http://localhost:9005/api/pricelist/get-non-active-pricelist/${this.Pharmacy}`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            usertype: this.userType,
+          },
+          method: "GET",
+        }
+      )
+        .then(function(response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return Promise.reject(response);
+          }
+        })
+        .then((data) => this.$store.dispatch("updatePriceListInactive", data))
+        .catch();
+    },
   },
-  mounted(){
-        this.getActivePriceList();
-  }
+  mounted() {
+    this.getActivePriceList();
+    this.getNonActivePriceList();
+  },
 };
 </script>
 <style scoped>
